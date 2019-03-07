a37d818ce9b1ad8f25fb5584e0e3ce596d05e119
Allen Boynton <alboynton4@gmail.com>
2019-03-07 17:14:46 -0500

    Renamed project
    Added images and completed score vc

--- duplicity files/Pods/Target Support Files/Pods-duplicityUITests/Pods-duplicityUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_duplicityUITests : NSObject
-@end
-@implementation PodsDummy_Pods_duplicityUITests
-@end
---
+++ twofold files/Pods/Target Support Files/Pods-twofoldUITests/Pods-twofoldUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_twofoldUITests : NSObject
+@end
+@implementation PodsDummy_Pods_twofoldUITests
+@end
