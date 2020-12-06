5fc7eccec877e10ca15a659e820c946a282986b8
Allen Boynton <alboynton4@gmail.com>
2020-12-05 21:09:32 -0500

    Create HomeView.swift
    Created sandbox HomeView as model Hosting Controller. Commented out.

---
+++ twofold files/twofold/SwiftUI Views/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  twofold
+//
+//  Created by Allen Boynton on 12/5/20.
+//  Copyright © 2020 Allen Boynton. All rights reserved.
+//
+
+//import SwiftUI
+//
+//struct HomeView: View {
+//    @available(iOS 13.0.0, *)
+//    var body: some View {
+//        Text("Home")
+//    }
+//}
+//
+//struct HomeView_Previews: PreviewProvider {
+//    @available(iOS 13.0.0, *)
+//    static var previews: some View {
+//        HomeView()
+//    }
+//}
+//
+//class HomeViewHostingController: UIHostingController<HomeView> {
+//    required init?(coder aDecoder: NSCoder) {
+//        super .init(rootView: HomeView())
+//    }
+//}
