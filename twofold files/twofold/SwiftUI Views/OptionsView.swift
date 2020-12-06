4275fa309911cd2bbe2e29f9af30ab54374be435
Allen Boynton <alboynton4@gmail.com>
2020-12-06 00:01:40 -0500

    Create OptionsView.swift

---
+++ twofold files/twofold/SwiftUI Views/OptionsView.swift
@@ -0,0 +1,24 @@
+//
+//  OptionsView.swift
+//  twofold
+//
+//  Created by Allen Boynton on 12/5/20.
+//  Copyright © 2020 Allen Boynton. All rights reserved.
+//
+
+import SwiftUI
+
+struct OptionsView: View {
+    @available(iOS 13.0.0, *)
+    var body: some View {
+        Text("Options View")
+            .fontWeight(.bold)
+    }
+}
+
+struct OptionsView_Previews: PreviewProvider {
+    @available(iOS 13.0.0, *)
+    static var previews: some View {
+        OptionsView()
+    }
+}
