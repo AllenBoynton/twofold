99bd47d0b142664355ca9d14e3a3a71b02edaad1
Allen Boynton <alboynton4@gmail.com>
2020-12-06 00:02:10 -0500

    Update DesignableButtons.swift

--- twofold files/twofold/View/DesignableButtons.swift
+++ twofold files/twofold/View/DesignableButtons.swift
@@ -10,31 +10,31 @@ import UIKit
 
 // Creates rounded buttons for choice buttons
 @IBDesignable class DesignableButtons: UIButton {
-    
+
     @IBInspectable var cornerRadius: CGFloat = 0.0 {
         didSet {
             self.layer.cornerRadius = cornerRadius
         }
     }
-    
+
     @IBInspectable var circle: CGFloat = 0.0 {
         didSet {
             self.layer.cornerRadius = self.frame.size.width / circle
         }
     }
-    
+
     @IBInspectable var borderWidth: CGFloat = 0.0 {
         didSet {
             self.layer.borderWidth = borderWidth
         }
     }
-    
+
     @IBInspectable var borderColor: UIColor = .clear {
         didSet {
             self.layer.borderColor = borderColor.cgColor
         }
     }
-    
+
     @IBInspectable var masksToBounds: Bool = false {
         didSet {
             self.layer.masksToBounds = masksToBounds
